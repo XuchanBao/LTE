a7bb20043f6225ad8c79a9804c1fe9538346f7b0
XuchanBao <touran123@outlook.com>
2020-04-29 18:24:54 -0400

    improve histogram visualization

--- src/visualizations.py
+++ src/visualizations.py
@@ -12,7 +12,7 @@ def histogram_overlayer(inv_distortion_values_dict, save_dir):
 
     # Create the plot.
     plt.close("all")
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(15, 5))
     # ____ Plot the histograms. ____
     for i, (k, v) in enumerate(inv_distortion_values_dict.items()):
         plt.hist(x=v, bins=50, color=COLORS[i], density=True, label=k, alpha=0.3)
@@ -24,9 +24,12 @@ def histogram_overlayer(inv_distortion_values_dict, save_dir):
         plt.axvline(v_np.min(), color=COLORS[i], linestyle='-.', linewidth=1, label=f"{k}/min")
 
     plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
-    plt.tight_layout()
     plt.xlim(-0.05, 1.05)
+    plt.ylim(0.001, 3000)
     plt.yscale('log')
+    plt.xlabel('inverse distortion ratio')
+    plt.ylabel('count')
+    plt.tight_layout()
     plt.savefig(os.path.join(save_dir, "ovelaid_histogram.png"))
 
 
